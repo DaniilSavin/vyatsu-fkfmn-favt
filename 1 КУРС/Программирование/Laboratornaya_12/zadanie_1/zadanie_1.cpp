2cf03221641359e394fb33d88ad69e32ccc2ca17
DaniilSavin <60922629+DaniilSavin@users.noreply.github.com>
2023-01-21 23:30:46 +0300

    Add files via upload

---
+++ 1 КУРС/Программирование/Laboratornaya_12/zadanie_1/zadanie_1.cpp
@@ -0,0 +1,23 @@
+﻿// zadanie_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string.h>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	char stroka='d';
+    int count = 0;
+	cout << "Введите строку: ";
+    while (stroka != '/')
+	{
+		cin >> stroka;
+		if (stroka == '!') count++;
+	}
+    cout << "Кол-во восклицателных знаков: "<< count << endl;
+}
