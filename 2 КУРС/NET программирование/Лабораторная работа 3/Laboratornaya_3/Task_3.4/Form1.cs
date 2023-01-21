7b18d80f8059934f4b5499472e17757e28bd0fa8
DaniilSavin <60922629+DaniilSavin@users.noreply.github.com>
2023-01-22 01:21:11 +0300

    Add files via upload

---
+++ 2 КУРС/NET программирование/Лабораторная работа 3/Laboratornaya_3/Task_3.4/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_3._4
+{
+
+
+    public partial class Form1 : Form
+    {
+
+        Graphics Graph;
+        Pen MyPen;
+        int x1;
+        int y1;
+        public Form1()
+        {
+            InitializeComponent();
+            Graph = CreateGraphics();
+            MyPen = new Pen(Color.Black,1);
+            MinimumSize = new Size(150, 150);   
+                int x = Size.Width;
+                int y = Size.Height;
+                Text = x + " x " + y;
+             x1 = Size.Width / 2;
+             y1= Size.Height / 2;
+            Graph.DrawEllipse(MyPen, x1,y1,500,500);
+            
+            
+        }
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Graph.DrawEllipse(MyPen, x1, y1, 30, 30);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            int x = Size.Width;
+            int y = Size.Height;
+            Text = x + " x " + y;
+            Graph.DrawEllipse(MyPen, x1, y1, 30, 30);
+        }
+    }
+}
---
+++ 2 КУРС/NET программирование/Лабораторная работа 3/Laboratornaya_3/Task_3.5/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_3._5
+{
+    public partial class Form1 : Form
+    {
+
+        Form f, f2, f3, f4;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar=='1')
+            {
+                f = new Form();
+                f.Show();
+            }
+            else
+            if (e.KeyChar == '2')
+            {
+                f = new Form();
+                f.Show();
+                f2 = new Form();
+                f2.Show();
+            }
+            else
+                if (e.KeyChar == '3')
+            {
+                f = new Form();
+                f.Show();
+                f2 = new Form();
+                f2.Show();
+                f3 = new Form();
+                f3.Show();
+            }
+            else
+            {
+                f = new Form();
+                f.Show();
+                f2 = new Form();
+                f2.Show();
+                f3 = new Form();
+                f3.Show();
+                f4 = new Form();
+                f4.Show();
+            }
+        }
+    }
+}
