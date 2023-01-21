8068e1ce2651963ccbcbb46c44ef0917054d145e
DaniilSavin <60922629+DaniilSavin@users.noreply.github.com>
2023-01-22 00:27:29 +0300

    Add files via upload

---
+++ 2 КУРС/NET программирование/_/Lab7/Num4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Num4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo fi1 = new FileInfo(@"C:\Users\zagos\source\repos\Num5\Num4\bin\Debug\text.txt");
+            if (!fi1.Exists)
+            {
+                Random rand = new Random();
+                StreamWriter sw = fi1.CreateText();
+                    for(int i = 0; i < 50; i++ )
+                    {
+                        int st = rand.Next(-50, 250);
+                        sw.WriteLine(st + " ");
+                    }
+                sw.Close();
+            }
+            FileStream fileIn = new FileStream("text.txt",
+                                    FileMode.Open,
+                                    FileAccess.Read);
+            byte[] array = new byte[fileIn.Length];
+            fileIn.Read(array, 0, array.Length);
+            string textFromFile = Encoding.Default.GetString(array);
+            //FileStream fileOut = new FileStream("newText.txt",
+            //                                     FileMode.Create,
+            //                                     FileAccess.Write);
+            Console.WriteLine(textFromFile);
+            string obSt ="";
+            StringBuilder sb = new StringBuilder(textFromFile);
+            for (int i = 0; i < sb.Length; i++)
+            {
+                if (sb[i] != '*' && sb[i] != '\n')
+                {
+                    obSt += sb[i];
+                    char ob = sb[i];
+                    for (int j = 0; j < sb.Length; j++)
+                    {
+                        if (sb[j] == ob)
+                        {
+                            sb[j] = '*';
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
---
+++ 2 КУРС/NET программирование/_/Lab7/Num6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Num6
+{
+    class Program
+    {
+        static void Directories(DirectoryInfo dir)
+        {
+            foreach (var y in dir.GetFiles())
+            {
+                string st = dir.FullName;
+                string g = "";
+                try
+                {
+                    string h = Convert.ToString(y.CreationTime);
+                    foreach(var x in h)
+                    {
+                        if (x == ' ')
+                        {
+                            g += '_';
+                        }
+                       else if (x == ':')
+                        {
+                            g += '_';
+                        }
+                        else g += x;
+                    }
+                    dir.CreateSubdirectory(g);
+                    st += "\\" + g;
+                    y.MoveTo(st + "\\" + y.Name);
+                }
+                catch
+                {
+                    st += "\\" + g;
+                    y.MoveTo(st + "\\" + y.Name);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\zagos\source\repos\Num5\Num6\pyp");
+           
+            Directories(dir);
+            Console.ReadKey();
+        }
+    }
+}
+
