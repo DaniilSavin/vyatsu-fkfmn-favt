2c757452049baf1c8b7cdb1174d47c7eb0571157
DaniilSavin <60922629+DaniilSavin@users.noreply.github.com>
2023-01-22 01:00:40 +0300

    Add files via upload

---
+++ 2 КУРС/NET программирование/Лабораторная 6/1.8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    private static int number;
+    static void Main()
+    {
+        bool fl = false;
+        while (!fl)
+        {
+            fl = true;
+            Console.Write("Введите трехзначное число: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number < 100 || number > 999)
+            {
+                Console.WriteLine("Неверные исходные данные.");
+                fl = false;
+            }
+        }
+        int x, y, z;
+        x = number % 10;
+        y = number / 10 % 10;
+        z = number / 100 % 10;
+        Console.WriteLine(z + " " + y + " " + x);
+            if (x != y)
+                Console.WriteLine(z + " " + x + " " + y);
+            if (y != 0 && y != z)
+            {
+                Console.WriteLine(y + " " + z + " " + x);
+                if (x != z)
+                    Console.WriteLine(y + " " + x + " " + z);
+            }
+
+            if (x != 0 && x != y && x != z)
+            {
+                Console.WriteLine(x + " " + y + " " + z);
+                if (y != z)
+                    Console.WriteLine(x + " " + z + " " + y);
+            }
+        Console.ReadKey();
+    }
+}
---
+++ 2 КУРС/NET программирование/Лабораторная 6/6,6/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task6
+{
+    class Program
+    {
+        private static bool flag = false;
+        private static int numberofcolumns = 0;
+        private static bool flagzero = false;
+        static void finding_a_column(int a, int b, int[,] arr) 
+        {
+            int count = 0;           
+            for (int j = 0; j < b; j++)
+            {
+                count = 0;
+                for (int i = 0; i < a; i++)
+                {
+                    if (arr[i, j] < 0) 
+                    {
+                        count++;
+                    }
+                    if (count > 2) 
+                    {
+                        i = b;
+                    }
+                }
+                if (count == 2) 
+                {
+                    
+                    Console.WriteLine("Cтолбец содержащий 2 отрицательных элемента: " + j );
+                    if (j == 0) flagzero = true;
+                    numberofcolumns += j;
+                    numberofcolumns *= 10;
+                    flag = true;
+                }
+            }
+            if (flag == false)  Console.WriteLine("Нет столбцов содержащих 2 отрицательных элемента");
+        }
+       
+        static void change(int[,] arr, int indexColumn)
+        {
+            if (indexColumn + 1 >= arr.GetLength(1))
+                return;
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = indexColumn; j < arr.GetLength(1) - 1; j++)
+                {
+                    int temp = arr[i, j];
+                    arr[i, j] = arr[i, j + 1];
+                    arr[i, j + 1] = temp;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Random R = new Random();
+            Console.WriteLine("Введите количество строк матрицы:");
+            int size = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов матрицы:");
+            int size1 = int.Parse(Console.ReadLine());
+            int[,] arr = new int[size, size1];
+            Console.WriteLine("Массив чисел:");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = R.Next(-50, 150);
+                    Console.Write("{0}\t", arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            finding_a_column(size, size1, arr);
+            numberofcolumns /= 10;
+
+            if (flag == true)
+            {
+                while (numberofcolumns > 0)
+                {
+                    if (flagzero == true) change(arr, 0);
+                    change(arr, numberofcolumns % 10);
+                    numberofcolumns /= 10;
+                }
+
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        Console.Write("{0}\t", arr[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadKey();
+        }        
+    }
+}
