2a9d59bc09dfb5819d00675e432443bd2c946d9a
DaniilSavin <60922629+DaniilSavin@users.noreply.github.com>
2023-01-21 23:36:43 +0300

    Add files via upload

---
+++ 1 КУРС/Программирование/Laboratornaya_12/zadanie_6_p2/zadanie_6_p2.cpp
@@ -0,0 +1,26 @@
+﻿// zadanie_6_p2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+	string str, symbols = "aeiouyAEIOUY";
+	getline(cin, str);
+	for (string::iterator it = str.begin(); it != str.end(); ++it)
+	{
+		if (find(symbols.begin(), symbols.end(), *it) != symbols.end())
+		{
+			str.erase(it);
+			it = str.begin();
+		}
+	}
+	cout << str << endl;
+
+	return 0;
+}
---
+++ 1 КУРС/Программирование/Laboratornaya_12/zadanie_7_p2/zadanie_7_p2.cpp
@@ -0,0 +1,25 @@
+﻿// zadanie_7_p2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+
+using namespace std; 
+
+int main()
+{
+	string str;
+	getline(cin, str);
+	stringstream ss(str);
+	int count = 1;
+	for (string word; ss >> word;)
+	{
+		if (count % 2 == 0)
+			reverse(word.begin(), word.end());
+		cout << word << " ";
+		count++;
+	}
+}
