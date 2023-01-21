e596c0b9aacd0e49badfbaceb6e7da73c3e53e7c
DaniilSavin <60922629+DaniilSavin@users.noreply.github.com>
2023-01-22 00:15:02 +0300

    Add files via upload

---
+++ 1 КУРС/УП/zadanie_889/zadanie_889/zadanie_889.cpp
@@ -0,0 +1,47 @@
+﻿// zadanie_889.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+//#include "pch.h"
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+struct Hor {
+	int x, y;
+};
+
+int main()
+{
+	int start, n;
+	std::cin >> start >> n;
+	std::vector<Hor> h(n);
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> h[i].x >> h[i].y;
+	}
+	std::sort(h.begin(), h.end(), [](const Hor &left, const Hor &right)
+	{
+		return left.y > right.y;
+	});
+	int cur = start;
+	for (Hor hor : h)
+	{
+		if (hor.x == cur)
+		{
+			cur++;
+
+		}
+		else
+		{
+			if (hor.x + 1 == cur)
+			{
+				cur--;
+			}
+		}
+		
+	}
+	std::cout << cur;
+}
+
+
---
+++ 1 КУРС/УП/zadanie_945/zadanie_945/zadanie_945.cpp
@@ -0,0 +1,40 @@
+﻿// zadanie_945.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+int main()
+{
+	int n, q;
+	std::cin >> n >> q;
+	std::vector<long long> a(n);
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> a[i];
+		
+	}
+	for (int i = 0; i < q; i++)
+	{
+		long long x;
+		std::cin >> x;
+		if (i > 0)
+		{
+			std::cout << std::endl;
+		}
+		if (std::binary_search(a.begin(), a.end(), x))
+		{
+			std::cout << "YES";
+		}
+		else
+		{
+			std::cout << "NO";
+		}
+	}
+	return 0;
+}
+
+
