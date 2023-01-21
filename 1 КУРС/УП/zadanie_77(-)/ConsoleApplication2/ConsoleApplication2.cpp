ed4e1d35c00a478c23e399bb5276dec29c93641a
DaniilSavin <60922629+DaniilSavin@users.noreply.github.com>
2023-01-22 00:07:28 +0300

    Add files via upload

---
+++ 1 КУРС/УП/zadanie_77(-)/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,54 @@
+﻿// ConsoleApplication5.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "pch.h"
+#include "stdlib.h"
+#include "math.h"
+#include <fstream>
+
+using namespace std;
+
+ifstream in("input.txt");
+ofstream out("output.txt");
+
+int main()
+{
+	int N, K;
+	int rezcount = 0;
+	in >> N >> K;
+	for (int i = 1; i <= N; i++)
+	{
+		int n = i;
+		int count = 0;
+		int c;
+		int* A;
+		int size = 0;
+		int ndublicate = n;
+		while (n != 0) {
+			c = n % 2;
+			n /= 2;
+			size++;
+		}
+		A = new int[size];
+		int g = 0;
+		while (ndublicate != 0) {
+			*(A + g) = ndublicate % 2;
+			ndublicate /= 2;
+			g++;
+		}
+		int result = 0;
+		for (g = 0; g < size; g++)
+		{
+			result += *(A + g)*(pow(10, g));
+		}
+		int temp = result;
+		while (temp != 0)
+		{
+			if (temp % 10 == 0) count++;
+			temp /= 10;
+		}
+		if (count == K) rezcount++;
+	}
+	out << rezcount;
+	return 0;
+}
---
+++ 1 КУРС/УП/zadanie_77(-)/zadanie_77/zadanie_77.cpp
@@ -0,0 +1,77 @@
+﻿// zadanie_77.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+void f(int n, int p, bool t, int K, bool tt);
+void mass(int n, int p, bool tt, int K);
+
+int arr[10000] = { 1 };
+int g = 0, count_ = 0;
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int N = 0, K = 0, count = 0;
+	int p = 2;
+	bool t = false;
+	bool tt = false;
+    cout << "Vvedite N and K: ";
+	cin >> N >> K;
+	for (int i = 1; i <= N; i++)
+	{
+		t = true;
+		f(i, p, t, K,tt);
+	}
+	cout << " " << endl;
+	for (int i = 0; i <= 16; i++)
+	{
+		cout << *(arr + i) << endl;
+	}
+	cout << count_ << endl;
+}
+
+void f(int n, int p, bool t, int K, bool tt)
+{
+	
+	int count = 0;
+	if (n > 0)
+	{
+		if (t==true)
+		{
+			cout << " " << endl;
+			cout << n << "-e число" << endl;
+			tt = true;
+		}
+		t = false;
+
+		f(n / p, p, t, K,tt);
+		
+		mass(n,p, tt, K);
+		
+		cout << ' ' << n % p;
+
+	}
+}
+
+void mass(int n, int p, bool tt, int K)
+{
+	
+		*(arr + g) = n % p;
+		g++;
+		if (tt == true)
+		{
+			for (int i = 0; i <= g; i++)
+			{
+				if (*(arr + i) == 0)
+				{
+					count_++;
+				}
+			}
+		}
+		tt = false;
+}
