2df1eadf05e23123f9ceb13cc8240976ddb36d6f
DaniilSavin <60922629+DaniilSavin@users.noreply.github.com>
2023-01-21 23:37:42 +0300

    Add files via upload

---
+++ 1 КУРС/Программирование/Laboratornaya_12/zadanie_8_p2/zadanie_8_p2.cpp
@@ -0,0 +1,16 @@
+﻿// zadanie_8_p2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <regex>
+#include <string>
+
+int main()
+{
+	std::string str;
+	std::getline(std::cin, str);
+	std::regex r("AEYUIOeyuioa");
+	std::cout << std::regex_replace(str, r, " ");
+	return 0;
+}
---
+++ 1 КУРС/Программирование/Laboratornaya_12/zadanie_9_p2/zadanie_9_p2.cpp
@@ -0,0 +1,30 @@
+﻿// zadanie_9_p2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <cctype>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string str = "abc";
+	getline(cin, str);
+	
+	unsigned int i = 0;
+	while (i < str.length())
+	{
+		if (isalpha(str[i]))
+		{
+			str.insert(i, "?");
+			++i;
+			while (i < str.length() && isalpha(str[i]))
+				++i;
+		}
+		else ++i;
+	}
+	cout << str << endl;
+	return 0;
+}
