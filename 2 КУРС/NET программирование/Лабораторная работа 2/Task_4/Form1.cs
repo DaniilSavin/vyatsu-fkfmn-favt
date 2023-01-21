4f2d5ee866b5d364b746cea058a4c0ffb0d7be51
DaniilSavin <60922629+DaniilSavin@users.noreply.github.com>
2023-01-22 01:15:06 +0300

    Add files via upload

---
+++ 2 КУРС/NET программирование/Лабораторная работа 2/Task_4/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_4
+{
+    public partial class Form1 : Form
+    {
+        Graphics Graph;
+        Pen MyPen;
+        int x, y;
+        int x1=0, y1=0;
+        bool draw=true;
+        public Form1()
+        {
+            InitializeComponent();
+            Graph = CreateGraphics();
+        }
+
+        private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+            y = e.Y;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                draw = true;
+                if (x1 == 0 && y1 == 0)
+                {
+                    x1 = e.X;
+                    y1 = e.Y;
+                }
+            }
+            else
+            {
+                Graph.DrawLine(MyPen, x1, y1, e.X, e.Y);
+                x1 = 0;
+                y1 = 0;
+                draw = false;
+            }
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            MyPen = new Pen(Color.Black, 1);
+            if (draw) Graph.DrawLine(MyPen, x, y, e.X, e.Y);
+            x = e.X;
+            y = e.Y;
+
+        }
+    }
+}
---
+++ 2 КУРС/NET программирование/Лабораторная работа 2/Task_5/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_5
+{
+    public partial class Form1 : Form
+    {
+
+        Graphics Graph;
+        Pen MyPen;
+        int x, y;
+        int x1, y1;
+        int switcher = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Graph = CreateGraphics();
+            MyPen = new Pen(Color.Black);
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            switch (switcher)
+            {
+                case 1:
+                    x = e.X;
+                    y = e.Y;
+                    switcher++;
+                    break;
+                case 2:
+                    x1 = e.X;
+                    y1 = e.Y;
+                    Graph.DrawLine(MyPen, x, y, e.X, e.Y);
+                    switcher++;
+                    break;
+                case 3:
+                    Graph.DrawLine(MyPen, x1, y1, e.X, e.Y);
+                    Graph.DrawLine(MyPen, e.X, e.Y, x, y);
+                    switcher = 1;
+                    break;
+
+            }
+
+        }
+    }
+}
+
