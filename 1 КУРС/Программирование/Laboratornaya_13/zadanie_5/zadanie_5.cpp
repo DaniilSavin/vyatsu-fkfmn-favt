55ad638b41ffbe8184c7088883cb1d85e255b0b6
DaniilSavin <60922629+DaniilSavin@users.noreply.github.com>
2023-01-21 23:39:33 +0300

    Add files via upload

---
+++ 1 КУРС/Программирование/Laboratornaya_13/zadanie_5/zadanie_5.cpp
@@ -0,0 +1,99 @@
+﻿// zadanie_5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <string>
+#include <cstdio>
+#include "ctime"
+#include "clocale"
+
+using namespace std;
+
+void file_save(char str[]);
+void newmass(char str[], int z);
+//void file_input(int n);
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	char *str = new char [100] ;
+	cout << "Введите ваш текст: ";
+	cin >> str;
+	file_save(str);
+	system("pause");
+	return 0;
+}
+
+void file_save(char str[])
+{
+	
+	int z = 0;
+	ofstream fout;
+	fout.open("f.txt");
+	
+	if (fout.is_open())
+	{
+		for (int i = 0; i < 100; i++)
+		{
+			if (*(str + i) == ' ' && i>0)
+			{
+				z = i;
+				newmass(str, z);
+			}
+		}
+	}
+	else
+	{
+		cout << "Ошибка открытия файла." << endl;
+	}
+	fout.close();
+	
+
+}
+
+/*void file_input(int n)
+{
+	string str;
+	int count_ = 0;
+	ifstream fin("f.txt");
+	ifstream gin("g.txt");
+	ofstream hout;
+	hout.open("h.txt", ofstream::app);
+	if (fin.is_open() && gin.is_open())
+	{
+		while (!fin.eof())
+		{
+			str = " ";
+			getline(fin, str);
+			hout << str << endl;
+		}
+		while (!gin.eof())
+		{
+			str = " ";
+			getline(gin, str);
+			hout << str << endl;
+		}
+
+	}
+	else
+	{
+		cout << "Ошибка открытия файла." << endl;
+	}
+	fin.close();
+	gin.close();
+	hout.close();
+}*/
+
+
+void newmass(char str[], int z)
+{
+	char *words = new char[100];
+	for (int h = 0; h < z; h++)
+	{
+		*(words + h) = *(str + h);
+	}
+}
---
+++ 1 КУРС/Программирование/Laboratornaya_13/zadanie_6/zadanie_6.cpp
@@ -0,0 +1,77 @@
+﻿// zadanie_6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+//#define _CRT_SECURE_NO_WARNINGS
+
+#include "pch.h"
+#include <iostream>
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include "clocale"
+
+using namespace std;
+
+char* strtolower(const char* str)
+{
+	char* res = new char[strlen(str) + 1];
+	strcpy(res, str);
+	for (size_t i = 0; i < strlen(res); ++i)
+		res[i] = tolower(res[i]);
+	return res;
+}
+
+
+int main()
+{
+	const int len = 255;
+	char word[len];
+	char line[len];
+	char *modline;
+	char* modword;
+	setlocale(LC_ALL, "Russian");
+
+
+	while (1)
+	{
+		cout << "Введите слово для поиска: ";
+		if (gets_s(word) == NULL) goto q;//ne nu eto ban
+
+		int l_word = strlen(word);
+		modword = strtolower(word);
+		ifstream fin("text.txt");
+		if (!fin)
+		{
+			cout << "Ошибка открытия файла." << endl;
+			return 1;
+		}
+
+		int count = 0;
+
+		while (!fin.eof()) // заносим строку длинной len в переменную line
+		{
+			fin.getline(line, len);
+			if (!strcmp(line, ""))
+				break;
+			modline = strtolower(line); //превращаем все прописные буквы в строчные
+			char *p = modline; // р - для хранения позиции за найденной подстрокой
+			while (p = strstr(p, modword)) // многократно ищем вхождение подстроки в строку
+			{
+				char *c = p; //c - для хранения адреса начала вхождения подстроки
+				p += l_word; // перемещаем указатель на длину заданного слова
+
+				if (c != modline) // слово не в начале строки?
+					if (!ispunct(*(c - 1)) && !isspace(*(c - 1))) // символ перед словом не разделитель?
+					
+				if (ispunct(*p) || isspace(*p) || (*p == '\0')) // символ после слова разделитель?
+					count++;
+			}
+		}
+		cout << endl << "Слово " << word << " встретилось в файле " << count << " раз(а). Выход <Ctrl+Z>" << endl << endl;
+
+	}
+q:;
+	system("pause");
+	return 0;
+}
