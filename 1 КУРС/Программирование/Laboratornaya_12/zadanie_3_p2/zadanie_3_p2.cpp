493869b05ea6faab1d77888d321529b5de6ab6cc
DaniilSavin <60922629+DaniilSavin@users.noreply.github.com>
2023-01-21 23:34:48 +0300

    Add files via upload

---
+++ 1 КУРС/Программирование/Laboratornaya_12/zadanie_3_p2/zadanie_3_p2.cpp
@@ -0,0 +1,41 @@
+﻿// zadanie_3_p2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string maxsesion(string str);
+
+int main()
+{
+	setlocale(0, "");
+	string str;
+	getline(cin, str);
+	cout << "result = " << maxsesion(str) << endl;
+	system("pause");
+	return 0;
+}
+
+string maxsesion(string str)
+{
+	int max = 0; char maxchar = '*'; int current = 0;
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (str[i] == str[i + 1])
+		{
+			current++;
+		}
+		if (current > max)
+		{
+			max = current;
+			maxchar = str[i];
+		}
+	}
+	str = "";
+	for (int i = 0; i <= max; i++) str += maxchar;
+	if (max > 0)return str; else return "ERROR!!!";
+}
+
---
+++ 1 КУРС/Программирование/Laboratornaya_12/zadanie_4/zadanie_4.cpp
@@ -0,0 +1,44 @@
+﻿// zadanie_4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string.h>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	bool t = false;
+	cout << "imput n: ";
+	int n;
+	cin >> n;
+	char *stroka = new char[n];
+	char c1 = 'd', c2 = 'c';
+
+	cout << "Введите строку: ";
+	cin >> stroka;
+	
+		cout << "Введите c1: ";
+		cin >> c1;
+
+		cout << "Введите c2: ";
+		cin >> c2;
+	
+
+	for (int i = 0; i <= n; i++)
+	{
+		if (*(stroka + i) == c1)
+		{
+			*(stroka + i) = c2;
+			t = true;
+		}
+	}
+	if (t = false)
+	{
+		cout << "Что-то пошло не так, или вы ввели неккоректные символы." << endl;
+	}
+	cout << "stroka -> " << stroka << endl;
+}
