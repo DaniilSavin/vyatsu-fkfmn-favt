b6ec5bf846d387c4e6eb251c4e2ac2ae859e9c94
DaniilSavin <60922629+DaniilSavin@users.noreply.github.com>
2023-01-21 23:35:47 +0300

    Add files via upload

---
+++ 1 КУРС/Программирование/Laboratornaya_12/zadanie_5/zadanie_5.cpp
@@ -0,0 +1,53 @@
+﻿// zadanie_5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string.h>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	int a, n;
+	int count = 0;
+	cout << "Каким способом? (1,2): ";
+	cin >> a;
+	switch (a) {
+	case 1:
+	{
+		cout << "RAZMER= ";
+		cin >> n;
+		char *stroka = new char[n];
+		
+		cout << "Введите строку: ";
+        cin >> stroka;
+		for (int i = 0; i <= n; i++)
+		{
+			if (*(stroka + i) == '1' || *(stroka + i) == '2' || *(stroka + i) == '3' || *(stroka + i) == '4' || *(stroka + i) == '5' || *(stroka + i) == '6' || *(stroka + i) == '7' || *(stroka + i) == '8' || *(stroka + i) == '9' || *(stroka + i) == '0')
+			{
+				count++;
+			}
+		}
+		cout << "Кол-во цифр: " << count << endl;
+		break;
+	}
+
+	case 2:
+		char stroka2 = 'd';
+		cout << "Введите строку(конечный символ /): ";
+		while (stroka2 != '/')
+		{
+			cin >> stroka2;
+			if (stroka2 == '0' || stroka2 == '1' || stroka2 == '2' || stroka2 == '3' || stroka2 == '4' || stroka2 == '5' || stroka2 == '6' || stroka2 == '7' || stroka2 == '8' || stroka2 == '9')
+			{
+				count++;
+			}
+		}
+		cout << "Кол-во цифр: " << count << endl;
+		break;
+	}
+}
+
---
+++ 1 КУРС/Программирование/Laboratornaya_12/zadanie_6/zadanie_6.cpp
@@ -0,0 +1,117 @@
+﻿// zadanie_6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string.h>
+#include <conio.h>
+#include <string> 
+
+using namespace std;
+
+int length(char s[]);
+int numbers(char str[]);
+int punct(char str[]);
+int prob(char str[]);
+int abc(char str[]);
+int other(char str[]);
+
+using namespace std;
+
+int lengthofstr = 0;
+
+int main()
+{
+	char s[80] = { 0 };
+	int count;
+	cout << "Введите строку: ";
+	gets_s(s);
+
+	cout << "length=" << length(s) << endl;
+	if (numbers(s) > 0)
+		cout << "numbers =" << numbers(s) << " " << numbers(s) * 100 / lengthofstr << "%" << endl;
+	else
+		cout << "numbers = 0 %0" << endl;
+	if (punct(s) > 0)
+		cout << "punct =" << punct(s) << " " << punct(s) * 100 / lengthofstr << "%" << endl;
+	else
+		cout << "punct = 0 %0" << endl;
+	if (punct(s) > 0)
+		cout << "prob =" << prob(s) << " " << prob(s) * 100 / lengthofstr << "%" << endl;
+	else
+		cout << "prob = 0 %0" << endl;
+
+	if (abc(s) > 0)
+		cout << "abc =" << abc(s) << " " << abc(s) * 100 / lengthofstr << "%" << endl;
+	else
+		cout << "abc = 0 %0" << endl;
+	if (other(s) > 0)
+		cout << "other =" << other(s) << " " << other(s) * 100 / lengthofstr << "%" << endl;
+	else
+		cout << "other = 0 %0" << endl;
+
+	system("pause");
+	return 0;
+}
+
+int length(char s[])
+{
+	for (int i = 0; i < 70 && (int)*(s + i) != 0 && (int)*(s + i) != -2; i++)
+	{
+		lengthofstr++;
+	}
+	return lengthofstr;
+}
+
+int abc(char str[])
+{
+	int count = 0;
+	int i = 0;
+	for (int i = 0; i < lengthofstr; i++)
+		if (isupper(str[i]) || islower(str[i]))count++;
+	return count;
+}
+
+int other(char str[])
+{
+	int count = 0;
+	int i = 0;
+	for (int i = 0; i < lengthofstr; i++)
+		if (!isdigit(str[i]) &&
+			!isalpha(str[i]) &&
+			!isspace(str[i]) &&
+			!isalnum(str[i]) &&
+			!ispunct(str[i]) &&
+			!isupper(str[i]) &&
+			!islower(str[i]))count++;
+	return count;
+}
+
+int prob(char str[])
+{
+	int count = 0;
+	int i = 0;
+	for (int i = 0; i < lengthofstr; i++)
+		if (str[i] == ' ')count++;
+	return count;
+}
+
+int punct(char str[])
+{
+	int count = 0;
+	int i = 0;
+	for (int i = 0; i < lengthofstr; i++)
+		if (ispunct(str[i]))count++;
+	return count;
+}
+
+int numbers(char str[])
+{
+	int count = 0;
+	int i = 0;
+	for (int i = 0; i < lengthofstr; i++)
+		if (isdigit(str[i]))count++;
+	return count;
+}
+
+
