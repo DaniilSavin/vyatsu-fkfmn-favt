59e6d8f1e88c09891c287a1a8b5a4b2bb0eba120
DaniilSavin <60922629+DaniilSavin@users.noreply.github.com>
2023-01-22 00:39:50 +0300

    Add files via upload

---
+++ 2 КУРС/NET программирование/_/_lab4/task8/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace task8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.YesButton = new System.Windows.Forms.Button();
+            this.NoButton = new System.Windows.Forms.Button();
+            this.QLabel = new System.Windows.Forms.Label();
+            this.YToolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // YesButton
+            // 
+            this.YesButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.YesButton.Location = new System.Drawing.Point(148, 310);
+            this.YesButton.Name = "YesButton";
+            this.YesButton.Size = new System.Drawing.Size(75, 23);
+            this.YesButton.TabIndex = 1;
+            this.YesButton.Text = "Да";
+            this.YToolTip.SetToolTip(this.YesButton, "Нажми сюда");
+            this.YesButton.UseVisualStyleBackColor = true;
+            this.YesButton.Click += new System.EventHandler(this.YesButton_Click);
+            // 
+            // NoButton
+            // 
+            this.NoButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NoButton.Location = new System.Drawing.Point(423, 310);
+            this.NoButton.Name = "NoButton";
+            this.NoButton.Size = new System.Drawing.Size(75, 23);
+            this.NoButton.TabIndex = 2;
+            this.NoButton.Text = "Нет";
+            this.NoButton.UseVisualStyleBackColor = true;
+            this.NoButton.LocationChanged += new System.EventHandler(this.NoButton_LocationChanged);
+            this.NoButton.Click += new System.EventHandler(this.NoButton_Click);
+            this.NoButton.Enter += new System.EventHandler(this.NoButton_Enter);
+            this.NoButton.MouseMove += new System.Windows.Forms.MouseEventHandler(this.NoButton_MouseMove);
+            // 
+            // QLabel
+            // 
+            this.QLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.QLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.QLabel.Location = new System.Drawing.Point(44, 165);
+            this.QLabel.Name = "QLabel";
+            this.QLabel.Size = new System.Drawing.Size(534, 39);
+            this.QLabel.TabIndex = 3;
+            this.QLabel.Text = "Вы довольны своей зарплатой?";
+            this.QLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // YToolTip
+            // 
+            this.YToolTip.Tag = "YesButton";
+            this.YToolTip.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 491);
+            this.Controls.Add(this.QLabel);
+            this.Controls.Add(this.NoButton);
+            this.Controls.Add(this.YesButton);
+            this.Name = "Form1";
+            this.Text = "Россиянская жизнь";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button YesButton;
+        private System.Windows.Forms.Button NoButton;
+        private System.Windows.Forms.Label QLabel;
+        private System.Windows.Forms.ToolTip YToolTip;
+    }
+}
+
---
+++ 2 КУРС/NET программирование/_/_lab4/task8/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task8
+{
+    public partial class Form1 : Form
+    {
+        Random Rand;
+        public Form1()
+        {
+            InitializeComponent();
+            Rand = new Random();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+
+        private void YesButton_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Close();
+            MessageBox.Show("Мы так и думали").ToString();
+        }
+
+        private void NoButton_Click(object sender, EventArgs e)
+        {
+            var rPoint = this.PointToClient(Cursor.Position);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+        
+        }
+
+        private void NoButton_LocationChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void NoButton_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+            /*int x1 = NoButton.Size.Width;
+            int y1 = NoButton.Size.Height;
+            int xb = NoButton.Location.X;
+            int yb = NoButton.Location.Y;
+            int n = Rand.Next(1, (ClientSize.Width-NoButton.Width));
+            int m = Rand.Next(1, (ClientSize.Height-NoButton.Height));
+            this.NoButton.Location = new Point(this.NoButton.Location.X, this.NoButton.Location.Y);
+            NoButton.Location = new Point( n,  m);*/
+            int x = Rand.Next(1, (ClientSize.Width - NoButton.Width));
+            int y = Rand.Next(1, (ClientSize.Height - NoButton.Height));
+            NoButton.Location = new Point(x, y);
+
+        }
+
+        private void NoButton_Enter(object sender, EventArgs e)
+        {
+            YesButton.Focus();
+        }
+    }
+}
