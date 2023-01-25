4cc58f3014ce566b6f0b621a8d3c50c8909c92f0
DaniilSavin <60922629+DaniilSavin@users.noreply.github.com>
2023-01-25 23:24:01 +0300

    Add files via upload

---
+++ 4 КУРС/1/Новая папка/asd/джава/simpleivy/src/in/ex/ivy/SimpleIvy.java
@@ -0,0 +1,10 @@
+package in.ex.ivy;
+
+import org.apache.commons.lang.StringUtils;
+
+public class SimpleIvy {
+    public static void main(String[] args) {
+           String string = StringUtils.upperCase("Ivy with Eclipse");
+           System.out.println(string);
+    }
+}
