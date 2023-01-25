a180d01be9b73d867b325a00b5c370a4df86067e
DaniilSavin <60922629+DaniilSavin@users.noreply.github.com>
2023-01-25 22:19:29 +0300

    Add files via upload

---
+++ 2 КУРС/ООП/6/6/MainWindow.xaml.cs
@@ -0,0 +1,375 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace _6
+{
+
+    public partial class MainWindow : Window
+    {
+        string leftop = "";
+        string op = "";
+        string rightop = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (UIElement c in LayoutRoot.Children)
+            {
+                if (c is Button)
+                {
+                    ((Button)c).Click += Button_Click;
+                }
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string s = (string)((Button)e.OriginalSource).Content;
+            if (textBlock.Text == "0")
+                textBlock.Text = "";
+            textBlock.Text += s;
+            int num;
+            bool result = Int32.TryParse(s, out num);
+            if (result)
+            {
+                if (op == "")
+                {
+                    leftop += s;
+                }
+                else rightop += s;
+            }
+            else
+            {
+                switch (s)
+                {
+                    case "=":
+                        Update_RightOp();
+                        if (op != "")
+                        {
+                            textBlock.Text = rightop;
+                            op = "";
+                            leftop = textBlock.Text;
+                            rightop = "";
+                        }
+                        break;
+                    case "CLEAR":
+                        leftop = "";
+                        rightop = "";
+                        op = "";
+                        textBlock.Text = "0";
+                        break;
+                    case "del":
+                        if (op != "" && rightop.Equals(""))
+                        {
+                            op = "";
+                            textBlock.Text = leftop;
+                        }
+                        else
+                        if (op == "" && !leftop.Equals(""))
+                        {
+                            leftop = leftop.Remove(leftop.Length - 1);
+                            textBlock.Text = leftop;
+                        }
+                        else if (!rightop.Equals(""))
+                        {
+                            rightop = rightop.Remove(rightop.Length - 1);
+                            textBlock.Text = leftop + op + rightop;
+                        }
+                        if (leftop.Equals("") || leftop.Equals(""))
+                        {
+                            textBlock.Text = "";
+                        }
+                        break;
+                    case "+/-":
+                        if (op == "")
+                        {
+                            if (leftop[0] != '-') 
+                                leftop = leftop.Insert(0, "-");
+                            else 
+                                leftop = leftop.Replace("-", " ");
+                            textBlock.Text = leftop;
+                        }
+                        else
+                        {
+                            if (rightop[0] != '-') 
+                                rightop = rightop.Insert(0, "-");
+                            else 
+                                rightop = rightop.Replace("-", " ");
+                            textBlock.Text = leftop + op + '(' + rightop + ')';
+                        }
+                        break;
+                    case "1/x":
+                        try
+                        {
+                            double number = double.Parse(leftop);
+                            if (number != 0)
+                            {
+                                leftop = Math.Pow(number, -1).ToString();
+                                textBlock.Text = leftop;
+                            }
+                            else 
+                                throw new ArgumentException();
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Деление на ноль");
+                            leftop = rightop = "";
+                            textBlock.Text = "0";
+                        }
+                        break;
+                    case "n!":
+                        try
+                        {
+                            double number = double.Parse(leftop);
+                            int digit = 1;
+                            for (int i = 2; i <= number; digit *= i, i++);
+                            if (digit < int.MaxValue) 
+                                leftop = digit.ToString();
+                            else 
+                                throw new ArgumentException();
+                            textBlock.Text = leftop;
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Слишком большое число");
+                            leftop = rightop = "";
+                            textBlock.Text = "0";
+                        }
+                        break;
+                    case "a^2":
+                        try
+                        {
+                            int number = int.Parse(leftop);
+                            int digit = number * number;
+                            if (digit < int.MaxValue) 
+                                leftop = digit.ToString();
+                            else 
+                                throw new ArgumentException();
+                            textBlock.Text = leftop;
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Слишком большое число");
+                            leftop = rightop = "";
+                            textBlock.Text = "0";
+                        }
+                        break;
+                    case "sqrt":
+                        try
+                        {
+                            double number = double.Parse(leftop);
+                            if (number >= 0)
+                            {
+                                leftop = Math.Sqrt(number).ToString();
+                                textBlock.Text = leftop;
+                            }
+                            else throw new ArgumentException();
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Введено отрицательное число");
+                            leftop = rightop = "";
+                            textBlock.Text = "0";
+                        }
+                        break;
+                    case "sin":
+                        try
+                        {
+                            double number = double.Parse(leftop);
+                            leftop = Math.Sin(number).ToString();
+                            textBlock.Text = leftop;
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Не корректное число");
+                            leftop = rightop = "";
+                            textBlock.Text = "0";
+                        }
+                        break;
+                    case "cos":
+                        try
+                        {
+                            double number = double.Parse(leftop);
+                            leftop = Math.Cos(number).ToString();
+                            textBlock.Text = leftop;
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Не корректное число");
+                            leftop = rightop = "";
+                            textBlock.Text = "0";
+                        }
+                        break;
+                    case "tan":
+                        try
+                        {
+                            double number = double.Parse(leftop);
+                            leftop = Math.Tan(number).ToString();
+                            textBlock.Text = leftop;
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Не корректное число");
+                            leftop = rightop = "";
+                            textBlock.Text = "0";
+                        }
+                        break;
+                    case "exp":
+                        leftop = Math.Exp(1).ToString();
+                        textBlock.Text = leftop;
+                        break;
+                    case "e^x":
+                        try
+                        {
+                            double number = double.Parse(leftop);
+                            if (Math.Exp(number) < double.MaxValue) leftop = Math.Exp(number).ToString();
+                            else throw new ArgumentException();
+                            textBlock.Text = leftop;
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Слишком большое число");
+                            leftop = rightop = "";
+                            textBlock.Text = "0";
+                        }
+                        break;
+                    case "pi":
+                        leftop = Math.PI.ToString();
+                        textBlock.Text = leftop;
+                        break;
+                    default:
+                        if (rightop != "")
+                        {
+                            Update_RightOp();
+                            leftop = rightop;
+                            rightop = "";
+                        }
+                        op = s;
+                        break;
+                }
+            }
+        }
+
+        private void Update_RightOp()
+        {
+            try
+            {
+                int number1 = int.Parse(leftop);
+                int number2 = int.Parse(rightop);
+                switch (op)
+                {
+                    case "+":
+                        try
+                        {
+                            if (number1 + number2 < int.MaxValue)
+                                rightop = (number1 + number2).ToString();
+                            else
+                                throw new ArgumentException();
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Слишком большое число");
+                            leftop = rightop = "";
+                            textBlock.Text = "0";
+                        }
+                        break;
+                    case "-":
+                        try
+                        {
+                            if (number1 - number2 > int.MinValue)
+                                rightop = (number1 - number2).ToString();
+                            else
+                                throw new ArgumentException();
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Слишком большое число");
+                            leftop = rightop = "";
+                            textBlock.Text = "0";
+                        }
+                        break;
+                    case "*":
+                        try
+                        {
+                            if (number1 * number2 < int.MaxValue)
+                                rightop = (number1 * number2).ToString();
+                            else
+                                throw new ArgumentException();
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Слишком большое число");
+                            leftop = rightop = "";
+                            textBlock.Text = "0";
+                        }
+                        break;
+                    case "/":
+                        try
+                        {
+                            if (number2 != 0)
+                                rightop = ((double)number1 / number2).ToString();
+                            else
+                                throw new ArgumentException();
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Деление на ноль");
+                            leftop = rightop = "";
+                            textBlock.Text = "0";
+                        }
+                        break;
+                    case "MOD":
+                        try
+                        {
+                            if (number2 != 0)
+                                rightop = (number1 % number2).ToString();
+                            else
+                                throw new ArgumentException();
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Деление на ноль");
+                            leftop = rightop = "";
+                            textBlock.Text = "0";
+                        }
+                        break;
+                    case "DIV":
+                        try
+                        {
+                            if (number2 != 0)
+                                rightop = Math.Truncate((double)number1 / number2).ToString();
+                            else
+                                throw new ArgumentException();
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Деление на ноль");
+                            leftop = rightop = "";
+                            textBlock.Text = "0";
+                        }
+                        break;
+                    case "^":
+                        try
+                        {
+                            if (Math.Pow(number1, number2) < int.MaxValue)
+                                rightop = Math.Pow(number1, number2).ToString();
+                            else
+                                throw new ArgumentException();
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Слишком большое число");
+                            leftop = rightop = "";
+                            textBlock.Text = "0";
+                        }
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                leftop = op = rightop = "";
+                textBlock.Text = "0";
+            }
+        }
+    }
+}
---
+++ 2 КУРС/ООП/6/_6/Program.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _6
+{
+    class Program
+    {
+        public class CalcLibrary
+        {
+            public class Calc
+            {
+                public delegate T OperationDelegate<T>(T x, T y);
+                public static Dictionary<string, OperationDelegate<double>> DoubleOperation = new Dictionary<string, OperationDelegate<double>>
+                {
+                    { "+", (x,y)=>x+y },
+                    { "-", (x,y)=>x-y },
+                    { "*", (x,y)=>x*y },
+                    { "/", (x,y)=>x/y },
+                    { "div", (x,y)=>(int)x/(int)y },
+                    { "mod", (x,y)=>x%y },
+                    { "x^", (x,y)=>Math.Pow(x,y) },
+                };
+                public delegate T OperationDelegate2<T>(T x);
+                public static Dictionary<string, OperationDelegate2<double>> DoubleOperation2 = new Dictionary<string, OperationDelegate2<double>>
+                {
+                    { "1/x", (x)=>1/x },
+                };
+
+
+                public static string DoOperation(string s)
+                {
+                    string op = GetOperation(s);
+                    string[] operands = GetOperands(s);
+                    return DoubleOperation[op](double.Parse(operands[0]), double.Parse(operands[1])).ToString();
+                }
+
+                public static string GetOperation(string s)
+                {
+                    String[] a = GetOperands(s);
+                    s = s.Replace(a[0], "");
+                    s = s.Replace(a[1], "");
+                    return s;
+                }
+                public static string[] GetOperands(string s)
+                {
+                    Regex rgx = new Regex(@"[^\d,\.\-]");
+                    MatchCollection mc = rgx.Matches(s);
+                    List<string> lm = new List<string>();
+                    foreach (Match m in mc)
+                    {
+                        lm.Add(m.Value);
+                    }
+                    return s.Split(lm.ToArray(), StringSplitOptions.None);
+                }
+
+            }
+        }
+        [TestMethod]
+        public static void CalculateTestMethod()
+        {
+            String[] a = CalcLibrary.Calc.GetOperands("23+4,5");
+            Assert.AreEqual("23", a[0]);
+            Assert.AreEqual("4,5", a[1]);
+        }
+        [TestMethod]
+        public static void ResultTestMethod()
+        {
+            Assert.AreEqual("27,5", CalcLibrary.Calc.DoubleOperation["+"](23, 4.5).ToString());
+            string result = CalcLibrary.Calc.DoOperation("23+4,5");
+            Assert.AreEqual("27,5", result);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalcLibrary.Calc.DoOperation("-23*0,5"));
+        }
+    }
+}
