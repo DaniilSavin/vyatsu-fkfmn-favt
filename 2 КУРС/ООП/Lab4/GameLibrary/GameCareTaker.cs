621343bb2e81e3145131fd17da619e35fd66f0aa
DaniilSavin <60922629+DaniilSavin@users.noreply.github.com>
2023-01-25 22:31:35 +0300

    Add files via upload

---
+++ 2 КУРС/ООП/Lab4/GameLibrary/GameCareTaker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary
+{
+    public class GameCaretaker
+    {
+        public Stack<GameMemento> states;
+
+        public GameCaretaker()
+        {
+            states = new Stack<GameMemento>();
+        }
+
+        public void Save(GameMemento state)
+        {
+            states.Push(state);
+        }
+
+        public GameMemento Restore()
+        {
+            if (states.Count == 0)
+                throw new ArgumentNullException();
+            GameMemento state = states.Pop();
+            return state;
+        }
+    }
+}
---
+++ 2 КУРС/ООП/Lab4/GameLibrary/GameMemento.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary
+{
+    public class GameMemento
+    {
+        internal int[,] field { get; }
+
+        internal int x0 { get; }
+
+        internal int y0 { get; }
+
+        internal int counter { get; }
+        public  GameMemento(int[,] field, int x0, int y0, int counter)
+        {
+            this.field = (int[,])field.Clone();
+            this.x0 = x0;
+            this.y0 = y0;
+            this.counter = counter;
+        }
+    }
+}
