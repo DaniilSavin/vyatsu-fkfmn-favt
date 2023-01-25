a40a6e6f282ec1fb142f0b4f3d395ae0867ed161
DaniilSavin <60922629+DaniilSavin@users.noreply.github.com>
2023-01-25 23:53:49 +0300

    Add files via upload

---
+++ 4 КУРС/2/programmnaya_inzheneria/лаб. Паттерн/Builder2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Builder2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
