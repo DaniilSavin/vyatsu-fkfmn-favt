602cbffce328834fdbeb22aec5b1b33d8cc15783
DaniilSavin <60922629+DaniilSavin@users.noreply.github.com>
2023-01-25 22:38:57 +0300

    Add files via upload

---
+++ 2 КУРС/ООП/Laboratornaya_6.1/UsingDelegates/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using CalcLibrary;
+namespace UsingDelegates
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.Write("Введите пример: ");
+            string a = Console.ReadLine();
+            Console.Write("Результат: ");
+            Console.WriteLine(Calc.DoOperation(a, true, a.First(), a.First()+1, a.Length-1)); 
+            Console.ReadKey();
+        }
+    }
+}
