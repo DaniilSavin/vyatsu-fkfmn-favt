d58f2e794e117c14776d3f9c35b0cf055ef3ef06
DaniilSavin <60922629+DaniilSavin@users.noreply.github.com>
2023-01-25 23:40:25 +0300

    Add files via upload

---
+++ 4 КУРС/1/Программная инженерия/1/Lab_2/ClassLibrary/Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Point
+    {
+        double _latitude, _longitude;
+
+        public double Latitude
+        {
+            get => _latitude;
+             set => _latitude = value;
+        }
+        public double Longitude
+        {
+            get => _longitude;
+             set => _longitude = value;
+        }
+
+        public Point(double latitude, double longitude)
+        {
+            this._latitude = latitude;
+            this._longitude = longitude;
+        }
+    }
+}
---
+++ 4 КУРС/1/Программная инженерия/1/Lab_2/zadanie_1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+namespace zadanie_1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double latitude = 1, longitude = 2;
+            Point point = new Point(latitude, longitude);
+            Assert.AreEqual (1, point.Latitude);
+
+        }
+        /*[TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void TestMethod2()
+        {
+
+        }*/
+        
+    }
+}
