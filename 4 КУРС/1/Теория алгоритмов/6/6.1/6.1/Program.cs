5ca277f82382fae997735626cc27258b0f6e2b5f
DaniilSavin <60922629+DaniilSavin@users.noreply.github.com>
2023-01-25 23:48:48 +0300

    Add files via upload

---
+++ 4 КУРС/1/Теория алгоритмов/6/6.1/6.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("-> ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= n; i++)
+            {
+                int x = 0;
+                int y = i;
+                while (y >= 0)
+                {
+                    Console.Write("{" + y + ";" + x + "} ");
+                    x++;
+                    y--;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
