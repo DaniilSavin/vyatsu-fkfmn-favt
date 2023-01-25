fa1ac2642fc46e8bc3c00b1e23f3d6b5bd3cd61b
DaniilSavin <60922629+DaniilSavin@users.noreply.github.com>
2023-01-25 22:53:25 +0300

    Add files via upload

---
+++ 2 КУРС/Программирование баз данных/SQLQuery1.sql
@@ -0,0 +1,50 @@
+Create table Parts
+(
+ PartID int primary key,
+ PartName char (100) not null,
+ HolderName char (100) not null,
+ CostOfThePart int not null,
+ ClientID int not null,
+);
+
+Create table PriceHistory
+(
+   PartID int primary key,
+   Date1 date not null,
+   HolderName char (100) not null,
+   Price int not null,
+);
+
+Create table Worker
+(
+   IDOfTheWorker int primary key,
+   EmployeeFullName char (100) not null,
+   DateOfJobAcceptance date not null,
+   DateOfCompletionOfWork date not null,
+   HourlyRate smallint not null,
+);
+
+Create table Transactions
+(
+    ClientID int primary key,
+ ThetransactionID int  not null,
+ TransactionDate datetime2 not null,
+ IDOfTheWorker int not null,
+);
+
+Create table Client
+(
+   ClientID int primary key,
+   ClientFullName char(100) not null,
+   DateOfBirth date not null,
+   TheAddressOfTheClient char(200) not null,
+   CustomerNumber bigint not null,
+
+);
+
+Create table Holder
+(
+   HolderName char(100) primary key,
+   AddressOfTheSupplier char(100) not null,
+   StandNumber bigint not null,
+);
